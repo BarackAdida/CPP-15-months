deca0ae4aadfa9caf99f2ffce0502f234907fb54
BarackAdida <adida.barack@student.moringaschool.com>
2025-11-05 02:12:49 +0300

    Continuation

--- intro.cpp
+++ intro.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 void arr() {
-    int arr[9];
     int t = 1;
 
     for (int i = 0; i < 10; i++) {
@@ -19,8 +19,16 @@ void sum() {
     cout << "The sum of numbers btw 1 and 100 is:" << sum << endl;
 }
 
+int calculations(int x, int y) {
+    int power = pow(y, 3);
+    float product = power * x * y;
+    return product;
+}
+
 int main () {
     arr();
     sum();
+    int result = calculations(4, 3);
+    cout << result << "\n";
     return 0;
 }
